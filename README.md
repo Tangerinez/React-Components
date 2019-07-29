e9141bb8f1a4f42cb715b2e3b6690b8e2167ee3f
Tangerinez <gtang159@gmail.com>
2019-07-29 00:02:18 -0700

    Added Button + Searchbar

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Components
-Useful React Components
---
+++ my-components/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "./components/Button";
+import SearchBar from "./components/SearchBar";
+
+const App = () => {
+  return (
+    <div>
+      Hi
+      <Button />
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
